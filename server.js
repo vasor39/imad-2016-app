1d76bc5dacb284bd648ca0a90c44b749c3a4a8cf
vasor39 <vasonaruto@gmail.com>
2016-10-05 18:15:28 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res){
-    res.sendFile(createTemplate(articleOne));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req, res){
