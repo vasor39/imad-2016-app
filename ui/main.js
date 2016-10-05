10ab2f649fc3fc578034409f01296c8be3399d09
vasor39 <vasonaruto@gmail.com>
2016-10-05 23:11:24 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+var element=document.getElementById('main-text');
+element.innerHTML='New Value';
+
+var img=document.getElementById('madi');
+
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick = function() {
+    var interval = setInterval(moveRight,100);
+  
+};
