09ecd5deaf18b7fdb0afec989746f2c1964a5fbb
vasor39 <vasonaruto@gmail.com>
2016-10-06 10:51:07 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,14 +2,14 @@
 var button=document.getElementById('counter');
 button.onclick=function(){
     //create the request
-    var request=new XMLHttprequest();
+    var request=new XMLHttpRequest();
     
     request.onreadystatechange=function(){
         if (request.readyState === XMLHttpRequest.DONE){
             //TAKE SOME ACTION
             if (request.status===200){
                 var counter = request.responseText;
-                var span=document.getElemenetById('count');
+                var span=document.getElementById('count');
                 span.innerHTML=counter.toString();
             }
         }
