c551b8f80a262d81556f0e3973a9d98bb4915501
vasor39 <vasonaruto@gmail.com>
2016-10-06 11:07:45 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,17 @@ button.onclick=function(){
     request.open('GET','http://vasor39.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    var names=['name1','name2','name3'];
+    var list='';
+    for (var i=0;i<names.length;i++){
+        list+='<li>'+names[i]+'</li>';
+    }
+    
+    var ul= document.getElementById('nameList');
+    ul.innerHTML=list;
+};
