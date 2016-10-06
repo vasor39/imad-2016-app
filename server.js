184707a346c228043aa9a811301bbb9ff5f8e00c
vasor39 <vasonaruto@gmail.com>
2016-10-06 11:12:23 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 var names=[];
-app.get('/submit-name/:name',function(){
+app.get('/submit-name/:name',function(req,res){
     // get the name of the request
     var name= req.params.name
     names.push(name);
