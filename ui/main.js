5d05572e53c37b7a1755ad027052478d3a5eecf1
vasor39 <vasonaruto@gmail.com>
2016-10-06 11:17:20 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,12 +24,20 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    var names=['name1','name2','name3'];
-    var list='';
-    for (var i=0;i<names.length;i++){
-        list+='<li>'+names[i]+'</li>';
-    }
+    var request=new XMLHttpRequest();
     
-    var ul= document.getElementById('nameList');
-    ul.innerHTML=list;
+    request.onreadystatechange=function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            //TAKE SOME ACTION
+            if (request.status===200){
+                var counter = request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+        //NOT DONE YET
+    };
+    //make the request
+    request.open('GET','http://vasor39.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
 };
