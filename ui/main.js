d63c025846e1ac9c18a3ede2d282224f7349ce4a
vasor39 <vasonaruto@gmail.com>
2016-10-06 10:44:15 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,21 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML='New Value';
-
-var img=document.getElementById('madi');
-
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight,50);
-  
+//counter code
+var button=document.getElementById('counter');
+button.onclick=function(){
+    //create the request
+    var request=new XMLHttprequest();
+    
+    request.onreadystatechange=function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            //TAKE SOME ACTION
+            if (request.status===200){
+                var counter = request.responseText;
+                var span=document.getElemenetById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+        //NOT DONE YET
+    };
+    //make the request
+    request.open('GET','http://vasor39.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
