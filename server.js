304dd3ae1ddba086a1f17ef029ba1c54149180fa
vasor39 <vasonaruto@gmail.com>
2016-11-07 19:07:44 +0200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
+var config={
+    user:"vasor39",
+    database:"vasor39",
+    host:"db.imad.hasura-app.io",
+    port:"5432",
+    password:process.env.DB_PASSWORD
+};
+
+var pool=new Pool(config);
 
 var app = express();
 app.use(morgan('combined'));
@@ -73,7 +84,15 @@ var htmlTemplate=`
 return htmlTemplate;
 }
 
-
+app.get('/test-db',function(req,res){
+    Pool.query("SELECT * FROM test",function(err,result){
+        if (err){
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    })    
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
